d49fd3cd3bd6268be7aa2d5858881fdb3000ad08
chinijo <juanantcejven@gmail.com>
2021-07-23 09:16:57 +0200

    1 packet sniffed

--- snif snif/sniffer.py
+++ snif snif/sniffer.py
@@ -2,7 +2,7 @@ import os
 import socket
 
 #host to listen
-HOST = '192.168.56.1'
+HOST = '192.168.1.106'
 
 def init():
     #bin to public interface and check for windows
@@ -11,6 +11,25 @@ def init():
     else:
         socket_protocol = socket.IPPROTO_ICMP
 
+    #def HOST IP and construct socket object w neccesary params
+    snifit = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
+    snifit.bind(HOST, 0)
+    
+    #socket that includes IP Heads of captured packages
+    snifit.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+    
+    #Windows? set network card to promiscous by sending IOCTL
+    if os.name == 'nt':
+        snifit.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+    
+    #read a packet
+    print(snifit.recvfrom(65565))
+    
+    #WIndows? turn off prom mode
+    if os.name == 'nt':
+        snifit.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+    
+    
 
 if __name__ == '__main__':
     init()  
