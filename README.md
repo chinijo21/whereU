12a8c19d6c7dcc882607fcec18a7882ec0ccc71b
chinijo <juanantcejven@gmail.com>
2021-07-25 11:47:58 +0200

    decoding IP Header

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # whereU
 #Mile1 
 Read a RAW IP HEADER
+
+we need only the format characters B (1-byte unsigned char), H (2-byte unsigned short), and s (a byte array that requires a byte-width specification; 4s means a 4-byte string)
+
+Of the first byte of header data we receive, we want to assign the ver variable only the high-order nybble, so right shift by 4 and it gives us the original byte
--- snif snif/sniffer.py
+++ snif snif/sniffer.py
@@ -1,10 +1,46 @@
 import os
 import socket
+import struct
+import sys
+import ipaddress
 
 #host to listen
 HOST = '192.168.1.106'
 
-def init():
+#1 maps the first 20bytes into a readable buffer
+class IP:
+    def __init__(self, buff=None):
+        header = struct.unpack('<BBHHHBBH4s4s', buff)
+        #assign to ver only the high order nybble(right shift by 4 places)
+        self.ver = header[0] >> 4
+        #second nibble doing AND (0xF = 00001111)
+        self.ihl = header[0] & 0xF
+        
+        #Now that is decoded we can get the info from the packet
+        self.ts = header[1]
+        self.len = header[2]
+        self.id = header[3]
+        self.off = header[4]
+        self.ttl = header[5]
+        self.proto_num = header[6]
+        self.sum = header[7]
+        self.who = header[8]
+        self.to = header[9]
+        
+        #readable ips
+        self.who_address = ipaddress.ip_address(self.who)
+        self.to_address = ipaddress.ip_address(self.to)
+        
+        #protocol constants -> 1: ICMP, 6:TCP, 17:UDP
+        self.proto_map = {1: 'ICMP', 6:'TCP', 17:'UDP'}
+        try:
+            self.proto = self.proto_map[self.proto_num]
+        
+        except Exception as e:
+            print('%s No protocol for %s' % (e, self.proto_num))
+            self.protocol = str(self.proto_num) 
+               
+def snifit():
     #bin to public interface and check for windows
     if os.name == 'nt':
         socket_protocol = socket.IPPROTO_IP
@@ -22,15 +58,24 @@ def init():
     if os.name == 'nt':
         snifit.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
     
-    #read a packet
-    print(snifit.recvfrom(65565))
-    
-    #WIndows? turn off prom mode
-    if os.name == 'nt':
-        snifit.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+    #read a packet till user stops
+    try:
+        while True:
+            buffer_og = snifit.recvfrom(65565)[0]
+            #IP header (20bytes)
+            ip_head = IP(buffer_og[0:20])
+            #print a readable mess
+            print('Protocol: %s %s -> %s' % (ip_head.proto,ip_head.who_address,ip_head.to_address))
+            
+    except KeyboardInterrupt:
+        #WIndows? turn off prom mode
+        if os.name == 'nt':
+            snifit.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+            sys.exit()
+        
     
     
 
 if __name__ == '__main__':
-    init()  
+    snifit()  
             
